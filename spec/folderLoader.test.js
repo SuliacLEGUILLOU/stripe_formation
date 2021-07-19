5018656e8e2bf9147cbba6d7500b04d0f4a38783
slg <suliac.leguillou@pm.me>
2021-07-19 16:45:21 +0200

    Skeleton for webhook management

--- spec/folderLoader.test.js
+++ spec/folderLoader.test.js
@@ -1,5 +1,8 @@
 const fl = require('../lib/folderLoader')
 
+const Path = require('path')
+const wh_path = Path.join(process.env.STRIPE_FORMATION_PATH, 'example/webhook')
+
 describe('folderLoader', () => {
     it('Should be a function', () => {
         expect(typeof(fl)).toBe('function')
@@ -8,7 +11,7 @@ describe('folderLoader', () => {
     describe('Loading folder', () => {
         it('should load file', async () => {
             var p = new Promise(resolve => {
-                fl('/home/su/Projects/stripe_formation/example/webhook/', (err, data) => {
+                fl(wh_path, (err, data) => {
                     expect(err).toBe(null)
                     expect(data).toEqual({
                         charges: {
